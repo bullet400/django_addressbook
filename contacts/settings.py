c73d5a7a39274de1e49014584d0ff25a280d22c8
bullet400 <amponsahbullet400@live.com>
2019-12-10 14:53:52 +0530

    Update settings

--- contacts/settings.py
+++ contacts/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from django_heroku
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -163,6 +163,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_URL ='login'
 LOGOUT_URL ='logout'
 LOGIN_REDIRECT_URL ='/home'
-
 
 django_heroku.settings(locals())
